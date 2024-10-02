e7cef5df861f125026939f1c20f05e56b457dd94
Aiskirin <derrickyukzs23@gmail.com>
2024-10-02 16:25:24 +0800

    Chase mechanic updated

--- Assets/Chase.cs
+++ Assets/Chase.cs
@@ -1,30 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Chase : MonoBehaviour
 {
     public GameObject player;
-    public float minSpeed;
-    public float maxSpeed;
-    public float speedMultiplier;
+    public float speed;
+    public float chaseRadius;
+    public float roamingRadius;
+    public Color detectionColor = Color.red;
 
+
+    private bool chaseState = false;
     private float distance;
-    private float speed;
+    private float detectionRadius;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        detectionRadius = roamingRadius;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         distance = Vector2.Distance(transform.position, player.transform.position);
-        speed = Mathf.Lerp(maxSpeed, minSpeed, distance * speedMultiplier);
-        
 
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-        speed = minSpeed + (1f / distance) * speedMultiplier;
+        if(distance <= detectionRadius && chaseState == false)
+        {
+            chaseState = true;
+            
+        }
+
+        if(chaseState==true)
+        {
+            detectionRadius = chaseRadius;
+            
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        
+        }
+
+        if (distance >= detectionRadius && chaseState == true)
+        {
+            chaseState = false;
+            detectionRadius = roamingRadius;
+
+        }
+
+
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = detectionColor;
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
     }
 }
