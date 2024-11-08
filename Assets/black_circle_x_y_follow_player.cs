57b9b440d6135535535c2d456ae8218ac2524a72
SiuNotXiu <anghengsiu123@gmail.com>
2024-11-08 15:01:46 +0800

    update_black_bubble

--- Assets/black_circle_x_y_follow_player.cs
+++ Assets/black_circle_x_y_follow_player.cs
@@ -8,7 +8,10 @@ public class black_circle_x_y_follow_player : MonoBehaviour
 
     private void OnValidate()
     {
-        transform.position = new Vector2(object_player.transform.position.x,
-                                         object_player.transform.position.y);
+        if (object_player != null)
+        {
+            transform.position = new Vector2(object_player.transform.position.x,
+                                             object_player.transform.position.y);
+        }
     }
 }
