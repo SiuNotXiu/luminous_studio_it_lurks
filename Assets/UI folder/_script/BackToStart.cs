5dce50ed0516d1617c4c3e6cc317f80c0b73a5a8
amfoof <tfdragon1122@gmail.com>
2024-11-21 23:14:07 +0800

    fixing bug for quit from journal

--- Assets/UI folder/_script/BackToStart.cs
+++ Assets/UI folder/_script/BackToStart.cs
@@ -18,6 +18,7 @@ public class BackToStart : MonoBehaviour
     private void Back()
     {
         ResetBGM();
+        InventoryController.JournalOpen = true;//true is close
         SceneManager.LoadScene("1st Scene");
     }
 
