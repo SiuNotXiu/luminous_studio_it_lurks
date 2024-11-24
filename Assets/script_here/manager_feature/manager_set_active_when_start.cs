6810cc4a83034d8fb007ba0217c15665d1af6ed9
amfoof <tfdragon1122@gmail.com>
2024-11-25 02:15:26 +0800

    key adding ongoing

--- Assets/script_here/manager_feature/manager_set_active_when_start.cs
+++ Assets/script_here/manager_feature/manager_set_active_when_start.cs
@@ -10,17 +10,23 @@ public class manager_set_active : MonoBehaviour
     //cj:here also use to active cornfield key random appear
 
     [SerializeField] List<GameObject> object_to_be_active;
-    [SerializeField] List<GameObject> cornfield_key;
+    public GameObject Bundle;
+    public Vector3 offset = new Vector3(1f, 0f, 0f);
 
     private void Start()
     {
+ 
         for (int i = 0; i <  object_to_be_active.Count; i++)
         {
             object_to_be_active[i].SetActive(true);
         }
 
-        int randomIndex = Random.Range(0, cornfield_key.Count);
-        cornfield_key[randomIndex].SetActive(true);
+        int random = Random.Range(1, 8);
+        GameObject environmentObject = GameObject.Find("Environment");
+        Transform cfcorpseTransform = environmentObject.transform.Find($"CornFieldCorpse/CFCorpse{random}");
+        Vector3 spawnPosition = cfcorpseTransform.position + offset;
 
+        // Spawn the key bundle
+        Instantiate(Bundle, spawnPosition, Quaternion.identity);
     }
 }
