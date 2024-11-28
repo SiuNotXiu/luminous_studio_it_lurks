f026ab87a673a9ffaa5932b432978ee2cc7f7f97
SiuNotXiu <anghengsiu123@gmail.com>
2024-11-29 00:20:04 +0800

    can close map with button now

--- Assets/UI folder/map/CloseMapBTN.cs
+++ Assets/UI folder/map/CloseMapBTN.cs
@@ -7,6 +7,15 @@ using UnityEngine.EventSystems;
 public class CloseMapBTN : MonoBehaviour
 {
     public Canvas canvas; // Reference to your Canvas
+    [SerializeField] public trigger_map_ui script_trigger_map_ui; // Reference to your Canvas
+
+    private void OnValidate()
+    {
+        if (script_trigger_map_ui == null)
+        {
+            script_trigger_map_ui = GameObject.Find("canvas_big_map").GetComponent<trigger_map_ui>();
+        }
+    }
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) // Detect left mouse click
@@ -27,12 +36,13 @@ public class CloseMapBTN : MonoBehaviour
         foreach (RaycastResult result in raycastResults)
         {
             // Log the clicked UI element or perform actions
-            Debug.Log("Clicked on: " + result.gameObject.name);
+            //Debug.Log("Clicked on: " + result.gameObject.name);
 
             // Example: Perform action on specific images
             if (result.gameObject.name == "CloseMapBTN")
             {
-                Debug.Log("Clicked on an image with tag 'Image': " + result.gameObject.name);
+                Debug.Log("Clicked on correct button");
+                script_trigger_map_ui.closemap();
             }
         }
     }
